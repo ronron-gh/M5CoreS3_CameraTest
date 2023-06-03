d4d844c717194fa8bd7369f8bf039af1b8e6acd4
ronron-gh <motohiro.sanpei@gmail.com>
2023-06-03 09:35:23 +0900

    Remove unused libraries

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,6 @@
 
 #include <M5Unified.h>
 #include <esp_camera.h>
-#include <gob_unifiedButton.hpp> // 2023/5/12現在 M5UnifiedにBtnA等がないのでGobさんのライブラリを使用
-gob::UnifiedButton unifiedButton;
 
 #define LCD_WIDTH (320)
 #define LCD_HEIGHT  (240)
@@ -83,7 +81,6 @@ void setup() {
   auto cfg = M5.config();
   cfg.output_power = true;
   M5.begin(cfg);
-  unifiedButton.begin(&M5.Display, gob::UnifiedButton::appearance_t::transparent_all);
 
   M5.Display.setFont(&fonts::efontJA_24);
   M5.Display.println("HelloWorld");
